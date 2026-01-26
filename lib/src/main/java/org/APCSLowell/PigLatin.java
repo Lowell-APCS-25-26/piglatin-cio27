1863bddcc20ac91f1a7ef7bc951a58803690de47
cio27 <scoobyokelly@gmail.com>
2026-01-26 05:50:24 +0000

    done

--- lib/src/main/java/org/APCSLowell/PigLatin.java
+++ lib/src/main/java/org/APCSLowell/PigLatin.java
@@ -2,16 +2,45 @@ package org.APCSLowell;
 
 public class PigLatin {
     public int findFirstVowel(String sWord) {
-        //precondition: sWord is a valid String of length greater than 0.
-        //postcondition: returns the position of the first vowel in sWord.  If there are no vowels, returns -1
-	    
+      for (int i = 0; i < sWord.length(); i++) {
+    String letter = sWord.substring(i, i + 1);
+    if (letter.equals("a") || letter.equals("e") || letter.equals("i") || letter.equals("o") || letter.equals("u") ||
+        letter.equals("A") || letter.equals("E") || letter.equals("I") || letter.equals("O") || letter.equals("U")) {
+        return i;
+    }
+}
         return -1;
     }
 
     public String pigLatin(String sWord) {
-        //precondition: sWord is a valid String of length greater than 0
-        //postcondition: returns the pig latin equivalent of sWord
-        
-        return null;
+    if (sWord.length() == 0) {
+    return sWord;
+}
+
+// Get the first letter
+String firstLetter = sWord.substring(0, 1);
+
+// Words that begin with a vowel
+if (firstLetter.equals("a") || firstLetter.equals("e") || firstLetter.equals("i") || firstLetter.equals("o") || firstLetter.equals("u") ||
+    firstLetter.equals("A") || firstLetter.equals("E") || firstLetter.equals("I") || firstLetter.equals("O") || firstLetter.equals("U")) {
+    return sWord + "way";
+}
+
+// Words that begin with "qu" or "Qu" or "QU"
+if (sWord.length() > 1) {
+    String firstTwo = sWord.substring(0, 2);
+    if (firstTwo.equals("qu") || firstTwo.equals("Qu") || firstTwo.equals("qU") || firstTwo.equals("QU")) {
+        return sWord.substring(2) + firstTwo + "ay";
+    }
+}
+
+// Words that are all consonants or begin with consonants
+int firstVowelIndex = findFirstVowel(sWord);
+if (firstVowelIndex == -1) {
+    return sWord + "ay"; // all consonants
+} else {
+    return sWord.substring(firstVowelIndex) + sWord.substring(0, firstVowelIndex) + "ay";
+}
+
     }
 }
